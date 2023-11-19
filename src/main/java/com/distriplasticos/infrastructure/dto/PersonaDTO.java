859cd9d760acecc3d071ad99cac61fc5d8fffcab
jose lozano tamayo <jose.lozano.tamayo@gmail.com>
2023-11-18 20:19:02 -0500

    files and clean code architecture is added

---
+++ src/main/java/com/distriplasticos/infrastructure/dto/PersonaDTO.java
@@ -0,0 +1,4 @@
+package com.distriplasticos.infrastructure.dto;
+
+public record PersonaDTO(Integer id, String name, String description, boolean status) {
+}
---
+++ src/test/java/com/distriplasticos/DistriplasticosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distriplasticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistriplasticosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
